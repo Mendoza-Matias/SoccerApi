6c33421f8d26bd8ded10c4a00799f203d40f8a57
Matias Mendoza <mendoza.e.matias@gmail.com>
2024-12-17 20:13:21 -0300

    Application configuration has been recreated

--- src/main/java/com/mmendoza/soccerapi/SoccerApiApplication.java
+++ src/main/java/com/mmendoza/soccerapi/SoccerApiApplication.java
@@ -1,4 +1,4 @@
-package com.mmendoza.soccerApi;
+package com.mmendoza.soccerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoccerApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoccerApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerApiApplication.class, args);
+    }
 
 }
