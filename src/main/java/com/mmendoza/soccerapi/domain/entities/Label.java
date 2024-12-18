e7779e15f8fe1c814df7ea464c7c842de7144afa
Matias Mendoza <mendoza.e.matias@gmail.com>
2024-12-18 20:18:53 -0300

    Finish ServCreateNewLabel and configuration database test

--- src/main/java/com/mmendoza/soccerapi/domain/entities/Label.java
+++ src/main/java/com/mmendoza/soccerapi/domain/entities/Label.java
@@ -1,18 +1,25 @@
 package com.mmendoza.soccerapi.domain.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "labels")
+
 @Getter
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Label {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer labelId;
 
+    @Column(name = "description", unique = true, nullable = false, length = 25)
     private String description;
 
     // private News news;
--- src/main/java/com/mmendoza/soccerapi/repository/ILabelRepository.java
+++ src/main/java/com/mmendoza/soccerapi/repository/ILabelRepository.java
@@ -1,7 +1,10 @@
 package com.mmendoza.soccerapi.repository;
 
+import com.mmendoza.soccerapi.domain.entities.Label;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ILabelRepository {
+public interface ILabelRepository extends JpaRepository<Label, Integer> {
+    Boolean existsByDescription(String description);
 }
