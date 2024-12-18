deb9897c386fbc75d51ddce30af932485474196b
Matias Mendoza <mendoza.e.matias@gmail.com>
2024-12-17 21:27:51 -0300

    We modify the tag mapper so that a bean is created

--- src/main/java/com/mmendoza/soccerapi/mappers/ILabelMapper.java
+++ src/main/java/com/mmendoza/soccerapi/mappers/ILabelMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ILabelMapper {
