03dbb3fd9d5e8d30624c8b56c34826391becda08
Matias Mendoza <mendoza.e.matias@gmail.com>
2024-12-18 07:55:59 -0300

    Add exception

--- src/main/java/com/mmendoza/soccerapi/services/label/ServCreateNewLabel.java
+++ src/main/java/com/mmendoza/soccerapi/services/label/ServCreateNewLabel.java
@@ -1,8 +1,20 @@
 package com.mmendoza.soccerapi.services.label;
 
+import com.mmendoza.soccerapi.domain.exception.PropertyException;
+import com.mmendoza.soccerapi.utils.GenericServices;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ServCreateNewLabel {
+public class ServCreateNewLabel extends GenericServices<String, Void> {
 
+    @Override
+    protected Void execute(String request) {
+
+        if (request.isEmpty())
+            throw new PropertyException("The description is empty");
+
+
+
+        return null;
+    }
 }
