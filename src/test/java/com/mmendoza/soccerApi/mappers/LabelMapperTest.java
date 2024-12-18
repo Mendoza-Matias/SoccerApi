e048e27726467fabb23263b0df9972da8e98e261
Matias Mendoza <mendoza.e.matias@gmail.com>
2024-12-17 21:28:19 -0300

    creation test label mapper

---
+++ src/test/java/com/mmendoza/soccerApi/mappers/LabelMapperTest.java
@@ -0,0 +1,42 @@
+package com.mmendoza.soccerapi.mappers;
+
+import com.mmendoza.soccerapi.domain.dto.label.LabelDto;
+import com.mmendoza.soccerapi.domain.entities.Label;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest /* Loads the application context and allows testing the mapper by injecting it. */
+public class LabelMapperTest {
+
+    @Autowired
+    private ILabelMapper labelMapper;
+
+    @Test
+    void toDto() {
+        LabelDto dto = labelMapper.
+                toDto(Label.builder()
+                        .labelId(1)
+                        .description("description")
+                        .build());
+
+        assertEquals(LabelDto.class, dto.getClass());
+        assertEquals(1, dto.getLabelId());
+        assertEquals("description", dto.getDescription());
+    }
+
+    @Test
+    void toEntity() {
+        Label entity = labelMapper
+                .toEntity(LabelDto.builder()
+                        .labelId(1)
+                        .description("description")
+                        .build());
+
+        assertEquals(Label.class, entity.getClass());
+        assertEquals(1, entity.getLabelId());
+        assertEquals("description", entity.getDescription());
+    }
+}
